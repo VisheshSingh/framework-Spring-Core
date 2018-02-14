f9c47559b9b8795c26f9bffd58e22e5591d799fa
Vishesh <visheshsingh16@gmail.com>
2018-02-14 16:46:17 -0600

    Setter injection in Spring

--- src/main/java/com/vishesh/framework_spring/CricketCoach.java
+++ src/main/java/com/vishesh/framework_spring/CricketCoach.java
@@ -4,7 +4,17 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 
+	public CricketCoach() {
+		System.out.println("Inside no-args constructor");
+	}
+
 	public CricketCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	// Setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Inside the setter method of Cricket Coach");
 		this.fortuneService = fortuneService;
 	}
 
---
+++ src/main/java/com/vishesh/framework_spring/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package com.vishesh.framework_spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve the bean
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+		// call the methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// Call the new methods
+		System.out.println(theCoach.getDailyFortune());
+
+		// close the context
+		context.close();
+
+	}
+
+}
