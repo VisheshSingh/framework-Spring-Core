145a87a5d3ba5b8e79f8440300efffd0d7f2bfa9
Vishesh <visheshsingh16@gmail.com>
2018-02-14 16:53:33 -0600

    Injection String literals

--- src/main/java/com/vishesh/framework_spring/CricketCoach.java
+++ src/main/java/com/vishesh/framework_spring/CricketCoach.java
@@ -4,6 +4,11 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 
+	// Setup private fields
+
+	private String emailAddress;
+	private String team;
+
 	public CricketCoach() {
 		System.out.println("Inside no-args constructor");
 	}
@@ -16,6 +21,26 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("Inside the setter method of Cricket Coach");
 		this.fortuneService = fortuneService;
+	}
+
+	// Getters and Setters of fields
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Cricket coach: Inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Cricket coach: Inside setter method - setTeam");
+		this.team = team;
 	}
 
 	public String getDailyWorkout() {
--- src/main/java/com/vishesh/framework_spring/SetterDemoApp.java
+++ src/main/java/com/vishesh/framework_spring/SetterDemoApp.java
@@ -13,9 +13,11 @@ public class SetterDemoApp {
 
 		// call the methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 
 		// Call the new methods
-		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 
 		// close the context
 		context.close();
